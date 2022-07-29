0c4c013bd4f1d55e0f6ee7616e2354393ff7e9a7
Marcos Albuquerque <desenvolvedorwebfrontend@gmail.com>
2022-07-28 23:05:32 -0300

    Initial commit.

--- src/main/java/com/trybe/acc/java/Lampada.java
+++ src/main/java/com/trybe/acc/java/Lampada.java
@@ -1,13 +1,19 @@
 package com.trybe.acc.java;
 
+
+/** Verifica o estado da Lâmpada. */
 public class Lampada {
 
-	public static void main(String[] args) {
-			// Seu código aqui
-						
-			// Imprime a saída no console. OBS: descomente a próxima linha
-			//System.out.println("A lampada ligada: " + isLigada + "!");
+  /** Imprime a saída no console. */ 
+  public static void main(String[] args) {
+    StringBuilder stringBuilder = new StringBuilder();
+    
+    Boolean isLigada = true;
+    StringBuilder frase = stringBuilder.append("A lampada ligada: ")
+                                       .append(isLigada).append("!");
 
-	}
+    System.out.println(frase);
+    
+  }
 
 }
